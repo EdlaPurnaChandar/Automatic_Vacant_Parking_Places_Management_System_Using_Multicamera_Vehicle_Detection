9b322bcba3e1608f2ffa4ee0f3f4174a97ddc07b
EdlaPurnaChandar <edlaravi121@gmail.com>
2026-04-06 12:20:32 +0530

    Create car.py

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, registration_number, driver_age):
+        self.registration_number = registration_number
+        self.driver_age = driver_age
+
+    def get_registration_number(self):
+        return self.registration_number
