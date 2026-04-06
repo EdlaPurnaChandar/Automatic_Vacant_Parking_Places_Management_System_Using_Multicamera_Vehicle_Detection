3ef4194c32ae73554481ce010b0b9d7fab3dd60f
EdlaPurnaChandar <edlaravi121@gmail.com>
2026-04-06 12:35:29 +0530

    Delete parking_ticket.py

--- parking_ticket.py
+++
@@ -1,7 +0,0 @@
-class ParkingTicket:
-    def __init__(self, car, parking_slot):
-        self.car = car
-        self.parking_slot = parking_slot
-
-    def get_parking_slot(self):
-        return self.parking_slot
